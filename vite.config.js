e8737221fba74e3e0994c313fcf406dd32df99bc
Karmz404 <fearmywepdotcom@gmail.com>
2023-09-24 23:27:42 +0800

    fjfjfj

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"portfolweb",
+  base:"/portfolweb/",
   plugins: [react()],
   server: {
     port: 7000, // Specify the port you want to use
